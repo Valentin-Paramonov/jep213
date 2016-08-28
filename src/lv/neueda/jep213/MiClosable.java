1419ca4bd814d76acb9d7d27e13f29e790524edb
Valentin-Paramonov <valentine.paramonov@gmail.com>
2016-08-28 06:55:04 +0300

    Update twr demo

---
+++ src/lv/neueda/jep213/MiClosable.java
@@ -0,0 +1,10 @@
+package lv.neueda.jep213;
+
+class MiClosable implements AutoCloseable {
+    void doStuff() {
+    }
+
+    @Override
+    public void close() throws Exception {
+    }
+}
--- src/lv/neueda/jep213/TryWithResourcesDemo.java
+++ src/lv/neueda/jep213/TryWithResourcesDemo.java
@@ -1,15 +1,10 @@
 package lv.neueda.jep213;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-
 class TryWithResourcesDemo {
     void tryThis() {
-        try (InputStream is = new FileInputStream(new File("notpornatall.mov"))) {
-            is.close();
-        } catch (IOException e) {
+        try (MiClosable closable = new MiClosable()) {
+            closable.doStuff();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
